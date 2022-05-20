3474bdaf864524d5cf9666b6a4e124ae77f03bce
Meddy <you@example.com>
2022-05-20 11:25:44 +0200

    test

--- src/components/Accueil.js
+++ src/components/Accueil.js
@@ -7,7 +7,7 @@ const Accueil = () => {
             <div className="max-w-[1040px] h-full grid md:grid-cols-2 gap-x-5 w-full mx-auto content-center">
                 <div className="bg-transparent flex flex-col justify-center md:items-start items-center">
                     <h1 className="text-secondaire pb-4">Rejoignez nous !</h1>
-                    <h1 className="font-bold text-5xl pb-10 text-white text-justify">Office center</h1>
+                    <h1 className="font-bold text-5xl pb-10 text-white text-justify">Office center      </h1>
                     <p className="pb-10 w-full text-white">
                         Eiusmod magna labore elit nisi fugiat sunt duis tempor incididunt mollit pariatur ad. Fugiat aliquip in velit cillum quis irure ex consectetur labore. Ex ipsum consectetur excepteur cillum laborum qui sit qui ea non cupidatat esse. Duis voluptate non nisi fugiat magna labore eiusmod veniam. Duis nisi proident velit ut magna ea do excepteur ex ex ad quis exercitation laborum.
                     </p>
