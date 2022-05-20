70483cbbeea27020fc3f060703c90eb80240da93
fabien <fabien3376@gmail.com>
2022-05-20 11:32:12 +0200

    [+] add vision

--- src/components/Vision.js
+++ src/components/Vision.js
@@ -11,8 +11,8 @@ const Vision = () => {
         <div className="flex flex-col justify-center items-center py-12">
             <h1 className="bg-black-500 pb-4 text-center font-bold text-3xl">Quelle est notre Vision ?</h1>
             <p className="pb-10 w-auto text-black text-center px-28 text-sm">
-                Office Center est une société nouvelle qui développe un concept original de centre d’affaires associant la domiciliation d’entreprise, la location de bureaux et le coworking dans un cadre de partage d’expérience et avec un rapport coût/service lowcost.
-                Office Center a pour objectif d’accompagner l’entreprise à sa création et durant les différentes étapes de son développement .
+                Office Center est une société nouvelle qui développe un concept original de centre d'affaires associant la domiciliation d'entreprise, la location de bureaux et le coworking dans un cadre de partage d'expérience et avec un rapport coût/service lowcost.
+                Office Center a pour objectif d'accompagner l'entreprise à sa création et durant les différentes étapes de son développement .
             </p>
             <h1 className="text-secondaire pb-12 text-center text-base">Les moyens OFFICE CENTER mis à la disposition de ses adhérents sont les suivants :</h1>
 
@@ -24,10 +24,10 @@ const Vision = () => {
                             Un emplacement exceptionnel au cœur de Lille :
                         </div>
                         <ul className="list-disc text-sm">
-                            <li>16-18 rue Faidherbe – 59000 LILLE</li>
+                            <li>16-18 rue Faidherbe - 59000 LILLE</li>
                             <li>250 mètres de la gare Lille Flandres, du métro et des bus</li>
                             <li>600 mètres de la gare Lille Europe</li>
-                            <li>30 minutes maximum de tous les centres économiques de l’Agglomération lilloise</li>
+                            <li>30 minutes maximum de tous les centres économiques de l'Agglomération lilloise</li>
                             <li>35 minutes de Bruxelles</li>
                             <li>45 minutes Aéroport Paris Roissy-CDG</li>
                             <li>1h10 de Paris</li>
