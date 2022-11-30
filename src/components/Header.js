54be50bff3ba2cc5adeb382763b1d97abe7c25f9
mscholz.dev <mscholz.dev@gmail.com>
2022-11-30 09:58:18 +0100

    start fixes

--- src/components/Header.js
+++ src/components/Header.js
@@ -29,95 +29,42 @@ const Header = ({ myRef, bgDark }) => {
   }, []);
 
   return (
-    <header ref={myRef}
-      aria-label="barre de navigation"
-      className={`z-50 w-full py-2 flex justify-between fixed h-auto p-8 md:px-44 top-0 left-0${ bgDark ? " bg-gray/70" : " bg-secondairecard/75" }`}
-    >
+    <header ref={myRef} aria-label="barre de navigation" className={`z-50 w-full py-2 flex justify-between fixed h-auto p-8 md:px-44 top-0 left-0${bgDark ? " bg-gray/70" : " bg-secondairecard/75"}`}>
       <div className="flex justify-start items-center">
         <Link to="/" className="pr-4">
-          <img
-            src={logo}
-            alt="représentation du logo de l'agence WorkUnion"
-            className="sm:h-10 sm:w-10 md:h-[70px] md:w-[70px] rounded-full"
-          />
+          <img src={logo} alt="représentation du logo de l'agence WorkUnion" className="sm:h-10 sm:w-10 md:h-[70px] md:w-[70px] rounded-full" />
         </Link>
       </div>
 
       <ul className="hidden md:flex items-center text-[16px]">
-        <Link
-          to="/#accueil"
-          className="p-4 font-light cursor-pointer hover:text-secondaire duration-300 text-black"
-          smooth
-        >
+        <Link to="/#accueil" className="p-4 font-light cursor-pointer hover:text-secondaire duration-300 text-zinc-700" smooth>
           Accueil
         </Link>
-        <Link
-          to="/#vision"
-          className="p-4 font-light cursor-pointer hover:text-secondaire duration-300 text-black"
-          smooth
-        >
+        <Link to="/#vision" className="p-4 font-light cursor-pointer hover:text-secondaire duration-300 text-zinc-700" smooth>
           Vision
         </Link>
-        <Link
-          to="/#tarif"
-          className="p-4 font-light cursor-pointer hover:text-secondaire duration-300 text-black"
-          smooth
-        >
+        <Link to="/#tarif" className="p-4 font-light cursor-pointer hover:text-secondaire duration-300 text-zinc-700" smooth>
           Tarifs
         </Link>
-        <Link
-          to="/#contact"
-          className="p-4 font-light cursor-pointer hover:text-secondaire duration-300 text-black"
-          smooth
-        >
+        <Link to="/#contact" className="p-4 font-light cursor-pointer hover:text-secondaire duration-300 text-zinc-700" smooth>
           Contact
         </Link>
       </ul>
-      <div
-        onClick={handleNav}
-        className="md:hidden cursor-pointer p-4 flex flex-row items-center justify-end"
-      >
-        {nav ? (
-          <AiOutlineClose size={20} className="text-white" />
-        ) : (
-          <AiOutlineMenu size={20} className="text-white" />
-        )}
+      <div onClick={handleNav} className="md:hidden cursor-pointer p-4 flex flex-row items-center justify-end">
+        {nav ? <AiOutlineClose size={20} className="text-white" /> : <AiOutlineMenu size={20} className="text-white" />}
       </div>
-      <div
-        className={
-          nav
-            ? "fixed left-0 top-[68px] w-[100%] h-full der-r-gray-100 ease-in-out duration-300"
-            : "ease-in-out duration-300 fixed left-[-100%] top-[68px]"
-        }
-        id="sidebar"
-      >
+      <div className={nav ? "fixed left-0 top-[68px] w-[100%] h-full der-r-gray-100 ease-in-out duration-300" : "ease-in-out duration-300 fixed left-[-100%] top-[68px]"} id="sidebar">
         <div className="flex flex-col text-[16px] bg-secondairecard/75">
-          <Link
-            to="#accueil"
-            className="p-4 border-b border-gray-300 cursor-pointer hover:text-secondaire duration-300 text-black"
-            smooth
-          >
+          <Link to="#accueil" className="p-4 border-b border-gray-300 cursor-pointer hover:text-secondaire duration-300 text-zinc-700" smooth>
             Accueil
           </Link>
-          <Link
-            to="#vision"
-            className="p-4 border-b border-gray-300 cursor-pointer hover:text-secondaire duration-300 text-black"
-            smooth
-          >
+          <Link to="#vision" className="p-4 border-b border-gray-300 cursor-pointer hover:text-secondaire duration-300 text-zinc-700" smooth>
             Vision
           </Link>
-          <Link
-            to="#tarif"
-            className="p-4 border-b border-gray-300 cursor-pointer hover:text-secondaire duration-300 text-black"
-            smooth
-          >
+          <Link to="#tarif" className="p-4 border-b border-gray-300 cursor-pointer hover:text-secondaire duration-300 text-zinc-700" smooth>
             Tarifs
           </Link>
-          <Link
-            to="#contact"
-            className="p-4 border-b border-gray-300 cursor-pointer hover:text-secondaire duration-300 text-black"
-            smooth
-          >
+          <Link to="#contact" className="p-4 border-b border-gray-300 cursor-pointer hover:text-secondaire duration-300 text-zinc-700" smooth>
             Contact
           </Link>
         </div>
--- src/components/racine/vidPres.js
+++ src/components/racine/vidPres.js
@@ -3,20 +3,9 @@ import video from "../../assets/back.mp4";
 const vidPres = () => {
   return (
     <div className="flex justify-center items-center">
-      <video
-        alt="Gare Lille Flandres, Grand Place de Lille"
-        src={video}
-        autoPlay
-        loop
-        muted
-        className="w-[100vw] h-auto max-h-[75vh] min-h-[400px] object-cover "
-        id="accueil"
-      />
+      <video alt="Gare Lille Flandres, Grand Place de Lille" src={video} autoPlay loop muted className="w-[100vw] h-auto max-h-[75vh] min-h-[400px] object-cover " id="accueil" />
       <h1 className="bg-white/70 absolute text-sm text-secondaire text-center flex flex-col gap-y-[16px] p-[24px] rounded-md">
-        <span className="text-[34px] title-bold">WORKUNION</span>{" "}
-        <span className="text-[18px]">
-          COWORKING - DOMICILIATION - LOCATION
-        </span>
+        <span className="text-[34px] ti²tle-bold">WORKUNION</span> <span className="text-[18px]">DOMICILIATION - BUREAUX - RÉPARATION</span>
       </h1>
     </div>
   );
